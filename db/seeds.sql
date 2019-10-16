b748ea66303e41205bdb2eeed6b5f08cd730b32a
Tetiana Cary <taniushka.tata@gmail.com>
2019-10-15 20:57:59 -0600

    Added some starters

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+--Seeds file to populate database with some starter data--
+
+INSERT INTO burgers (burger_name, devoured) VALUES ("Fantasy Burger", true);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ("Old Fashion Burger", false);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ("Mango Burger", true);
