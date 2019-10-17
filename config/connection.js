3fa47c5c580ff802a7c1d714ccdb4a89dafcfd07
Tetiana Cary <taniushka.tata@gmail.com>
2019-10-17 09:30:12 -0600

    Updated the connection

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,17 @@
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 8889,
-    user: "root",
-    password: "root",
-    database: "burger_db"
-});
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 8889,
+        user: "root",
+        password: "root",
+        database: "burger_db"
+    });
+}
 
 
 connection.connect(function (err) {
