97df796d8df1d1983981c3cc7e44ce6f4320ba4c
Tetiana Cary <taniushka.tata@gmail.com>
2019-10-17 08:50:49 -0600

    Added logic to the burger.js

---
+++ models/burger.js
@@ -0,0 +1,36 @@
+// Import the ORM to create functions that will interact with the database.
+var orm = require("../config/orm.js");
+
+var burger = {
+    //Select all burgers from database.
+    all: function (cb) {
+        orm.all("burgers", function (res) {
+            cb(res);
+        });
+    },
+
+    //Create function to create/add a burger.
+    // The variables cols and vals are arrays.
+    create: function (cols, vals, cb) {
+        orm.create("burgers", cols, vals, function (res) {
+            cb(res);
+        });
+    },
+
+    //Update function to update burger devoured state.
+    update: function (objColVals, condition, cb) {
+        orm.update("burgers", objColVals, condition, function (res) {
+            cb(res);
+        });
+    },
+
+    //Delete function to throw away/delete burger from database.
+    delete: function (condition, cb) {
+        orm.delete("burgers", condition, function (res) {
+            cb(res);
+        });
+    }
+};
+
+// Export the database functions for the controller (burgers_controller.js).
+module.exports = burger;
